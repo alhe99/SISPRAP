eb8ebf7a8d753861e1517487114cc10096ab88e9
Christian <chrishernan146@gmail.com>
2019-01-02 16:51:11 -0600

    atajos

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,21 +6,54 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-12 text-center">
-                            <img src="images/logo.png" width="1050" class="img-fluid" alt="logo sisprap">
+                        <img src="images/logo.png" class="img-fluid" alt="logo sisprap" style="width: 700px;">
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="col-lg-4 col-md-4">
+        <div class="card  text-center">
+         {{--    <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12 text-center"> --}}
+                        <img src="images/atajos/GESTIONES.png" class="img-fluid" alt="logo sisprap" style="width: 410px; height: 170px; display: block; margin-left: auto; margin-right: auto;">
+            {{--         </div>
+                </div>
+            </div> --}}
+        </div>
+    </div>
+    <div class="col-lg-4 col-md-4">
+        <div class="card text-center">
+            {{-- <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12 text-center"> --}}
+                        <img src="images/atajos/PEINSCRIPCIONES.png" class="img-fluid" alt="logo sisprap" style="width: 410px; height: 170px; display: block; margin-left: auto; margin-right: auto;">
+           {{--          </div>
+                </div>
+            </div> --}}
+        </div>
+    </div>
+    <div class="col-lg-4 col-md-4">
+        <div class="card text-center">
+      {{--       <div class="card-body">
+                <div class="row">
+                    <div class="col-md-12 text-center"> --}}
+                        <img src="images/atajos/PUBLICACIONES.png" class="img-fluid" alt="logo sisprap" style="width: 410px; height: 170px;display: block; margin-left: auto; margin-right: auto;">
+          {{--           </div>
+                </div>
+            </div> --}}
+        </div>
+    </div>
 </template>
 @if (Auth::user()->rol_id == 1)
-    <template v-if="menu==1">
-        <publiproject></publiproject>
-    </template>
+<template v-if="menu==1">
+    <publiproject></publiproject>
+</template>
 @elseif(Auth::user()->rol_id == 2)
-    <template v-if="menu==1">
-        <pagoarancel></pagoarancel>
-    </template>
+<template v-if="menu==1">
+    <pagoarancel></pagoarancel>
+</template>
 @endif
 <template v-if="menu==2">
     <preregister></preregister>
@@ -56,24 +89,24 @@
     <carrinst></carrinst>
 </template>
 <template v-if="menu==13">
-   <supervision></supervision>
+ <supervision></supervision>
 </template>
 <template v-if="menu==14">
-   <regsuperv></regsuperv>
+ <regsuperv></regsuperv>
 </template>
 <template v-if="menu==15">
-   <configuracion></configuracion>
+ <configuracion></configuracion>
 </template>
 <template v-if="menu==16">
-   <inicioproceso></inicioproceso>
+ <inicioproceso></inicioproceso>
 </template>
 <template v-if="menu==17">
-   <pendientesinicio></pendientesinicio>
+ <pendientesinicio></pendientesinicio>
 </template>
 <template v-if="menu==18">
-   <pendientefin></pendientefin>
+ <pendientefin></pendientefin>
 </template>
 <template v-if="menu==19">
-   <culminados></culminados>
+ <culminados></culminados>
 </template>
 @endsection
