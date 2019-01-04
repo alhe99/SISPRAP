d4c51597a44f7e19acbc710977b6305e0046609a
Christian <chrishernan146@gmail.com>
2019-01-04 11:24:22 -0600

    Atajo solo para Admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,73 +1,76 @@
 @extends('admin.layout.app')
 @section('contenido')
 <template v-if="menu==0">
-    <div class="col-lg-12 col-md-12">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <img src="images/logo.png" class="img-fluid" alt="logo sisprap" style="width: 800px;">
-                    </div>
+   <div class="col-lg-12 col-md-12">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <img src="images/logo.png" class="img-fluid" alt="logo sisprap" style="width: 800px;">
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-lg-4 col-md-4">
-        <div class="card">
-            <img class="card-img-top"  src="images/atajos/GESTIONES.png" @click="menu=4" style="cursor: pointer" alt="Card image cap">
-            <div class="card-img-overlay" style="height:90px;">
-            </div>
-            <div class="card-body weather-small">
-                <div class="row">
-                    <div class="col-12 b-r align-self-center">
-                        <div class="d-flex">
-                            <div class="m-l-20 text-center" @click="menu=4"  style="display: block; cursor:pointer;margin-left: auto;margin-right: auto;">
-                                <h6><strong>GESTION PROYECTOS</strong></h6>
-                            </div>
+</div>
+@if (Auth::user()->rol_id == 1)
+<div class="col-lg-4 col-md-4">
+    <div class="card">
+        <img class="card-img-top"  src="images/atajos/GESTIONES.png" @click="menu=4" style="cursor: pointer" alt="Card image cap">
+        <div class="card-img-overlay" style="height:90px;">
+        </div>
+        <div class="card-body weather-small">
+            <div class="row">
+                <div class="col-12 b-r align-self-center">
+                    <div class="d-flex">
+                        <div class="m-l-20 text-center" @click="menu=4"  style="display: block; cursor:pointer;margin-left: auto;margin-right: auto;">
+                            <h6><strong>GESTION PROYECTOS</strong></h6>
                         </div>
+                    </div>
 
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-lg-4 col-md-4">
-        <div class="card">
-            <img class="card-img-top"  src="images/atajos/PEINSCRIPCIONES.png" @click="menu=2" style="cursor: pointer" alt="Card image cap">
-            <div class="card-img-overlay" style="height:90px;">
-            </div>
-            <div class="card-body weather-small">
-                <div class="row">
-                    <div class="col-12 b-r align-self-center">
-                        <div class="d-flex">
-                            <div class="m-l-20 text-center" @click="menu=2"  style="display: block; cursor:pointer;margin-left: auto;margin-right: auto;">
-                                <h6><strong>PREINSCRIPCIONES</strong></h6>
-                            </div>
+</div>
+<div class="col-lg-4 col-md-4">
+    <div class="card">
+        <img class="card-img-top"  src="images/atajos/PEINSCRIPCIONES.png" @click="menu=2" style="cursor: pointer" alt="Card image cap">
+        <div class="card-img-overlay" style="height:90px;">
+        </div>
+        <div class="card-body weather-small">
+            <div class="row">
+                <div class="col-12 b-r align-self-center">
+                    <div class="d-flex">
+                        <div class="m-l-20 text-center" @click="menu=2"  style="display: block; cursor:pointer;margin-left: auto;margin-right: auto;">
+                            <h6><strong>PREINSCRIPCIONES</strong></h6>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-lg-4 col-md-4">
-        <div class="card">
-            <img class="card-img-top"  src="images/atajos/PUBLICACIONES.png" style="cursor: pointer" @click="menu=1"  alt="Card image cap">
-            <div class="card-img-overlay" style="height:90px;">
-            </div>
-            <div class="card-body weather-small">
-                <div class="row">
-                    <div class="col-12 b-r align-self-center">
-                        <div class="d-flex">
-                            <div class="m-l-20 text-center" @click="menu=1" style="display: block;  cursor:pointer;margin-left: auto;margin-right: auto;">
-                                <h6><strong>PUBLICACIONES</strong></h6>
-                            </div>
+</div>
+<div class="col-lg-4 col-md-4">
+    <div class="card">
+        <img class="card-img-top"  src="images/atajos/PUBLICACIONES.png" style="cursor: pointer" @click="menu=1"  alt="Card image cap">
+        <div class="card-img-overlay" style="height:90px;">
+        </div>
+        <div class="card-body weather-small">
+            <div class="row">
+                <div class="col-12 b-r align-self-center">
+                    <div class="d-flex">
+                        <div class="m-l-20 text-center" @click="menu=1" style="display: block;  cursor:pointer;margin-left: auto;margin-right: auto;">
+                            <h6><strong>PUBLICACIONES</strong></h6>
                         </div>
+                    </div>
 
-                    </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+@endif
+
 {{--     <div class="col-lg-12 col-md-12">
         <div class="row">
             <div class="col-lg-4 col-md-4">
