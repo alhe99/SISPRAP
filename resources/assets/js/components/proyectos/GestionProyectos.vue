f4776c1b5e0ccdc36134093f3fcb06f8322ef296
Christian <chrishernan146@gmail.com>
2019-01-25 10:15:10 -0600

    hola que hace :cat:

--- resources/assets/js/components/proyectos/GestionProyectos.vue
+++ resources/assets/js/components/proyectos/GestionProyectos.vue
@@ -26,7 +26,7 @@
                       </div>
                     </div>
                   </div>
-                </fieldset>
+                </fieldset> 
               </div>
             </div>
           </div>
