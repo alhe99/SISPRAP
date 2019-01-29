052937fca8c6884d4c00958d6e3ecbefb05392af
Alonsohdesz <luisalonso123421@gmail.com>
2019-01-29 17:11:59 -0600

    Cambio de ruta de reporte de perfil

--- routes/web.php
+++ routes/web.php
@@ -284,3 +284,18 @@ Route::get('/test', function () {
     return Auth::user()->estudiante->gestionproyecto[0]->proyecto;
 
 });
+
+Route::get('/_debugbar/assets/stylesheets', [
+    'as' => 'debugbar-css',
+    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@css'
+]);
+
+Route::get('/_debugbar/assets/javascript', [
+    'as' => 'debugbar-js',
+    'uses' => '\Barryvdh\Debugbar\Controllers\AssetController@js'
+]);
+
+Route::get('/_debugbar/open', [
+    'as' => 'debugbar-open',
+    'uses' => '\Barryvdh\Debugbar\Controllers\OpenController@handler'
+]);
